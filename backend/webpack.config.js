19f5ed21dfd45f7e9670c7dbc6d873af79a6a9f0
NgyenPHuy <nguyenphuhuy180920@gmail.com>
2022-12-07 11:43:49 +0700

    bugfix: deploy backend
    bugfix: deploy backend

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path')
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: './app.js',
   target: 'node',
   mode: 'development',
+  externals: [nodeExternals()],
+  externalsPresets: {
+      node: true
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js',
