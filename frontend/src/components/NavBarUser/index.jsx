398fc60f433378e5075658b8a19ad17e218ec03a
NgyenPHuy <nguyenphuhuy180920@gmail.com>
2022-12-09 17:26:12 +0700

    bugfix: fix login google

--- frontend/src/components/NavBarUser/index.jsx
+++ frontend/src/components/NavBarUser/index.jsx
@@ -3,15 +3,32 @@ import './style.scss'
 import { userLogOut } from '@app/redux/login/actions'
 import { Dropdown, Space, Avatar, Button } from 'antd'
 import { useCallback } from 'react'
+import { useGoogleLogout } from 'react-google-login'
 import { useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const NavBarUser = () => {
   const dispatch = useDispatch()
   const logOut = useCallback(() => dispatch(userLogOut()), [dispatch])
+
+  const navigate = useNavigate()
+  const onLogoutSuccess = () => {
+    navigate('/auth/login')
+  }
+
+  const onFailure = () => {
+    console.error('logout fail')
+  }
+
+  const { signOut } = useGoogleLogout({
+    onFailure,
+    clientId: import.meta.env.VITE_CLIENT_ID,
+    onLogoutSuccess,
+  })
   const handleLogOut = () => {
     localStorage.removeItem('token')
     logOut()
+    signOut()
   }
   const items = [
     {
@@ -25,10 +42,6 @@ const NavBarUser = () => {
         </Link>
       ),
       key: 'user2',
-    },
-    {
-      label: <Link to="/auth/login">Login</Link>,
-      key: 'user3',
     },
   ]
   return (
