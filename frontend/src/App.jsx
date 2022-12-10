92799ba228431423de4fd1b197ca49e8974d785a
NgyenPHuy <nguyenphuhuy180920@gmail.com>
2022-12-10 19:29:53 +0700

    feat: route admin and client
    feat: route admin and client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import '@app/App.css'
 import NotFound from '@app/components/NotFound/NotFound'
 import Index from '@app/pages/Admin'
 import Auth from '@app/pages/Auth'
+import Client from '@app/pages/Client'
 import { Routes, Route } from 'react-router-dom'
 const App = () => (
   <>
     <Routes>
       <Route element={<Index />} path="admin/*" />
+      <Route element={<Client />} path="client/*" />
       <Route element={<Auth />} path="auth/*" />
       <Route element={<NotFound />} path="*" />
     </Routes>
