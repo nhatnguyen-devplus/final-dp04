f8b9493909765896aece6024fec46ccdaad1339a
NgyenPHuy <nguyenphuhuy180920@gmail.com>
2022-12-10 18:52:11 +0700

    feature/route-auth

--- frontend/src/pages/Auth/index.jsx
+++ frontend/src/pages/Auth/index.jsx
@@ -5,17 +5,21 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './Login'
 const Auth = () => {
-  const { isLoggedIn } = useSelector((state) => state.login)
+  const { isLoggedIn, isRole, loading } = useSelector((state) => state.login)
   const dispatch = useDispatch()
   const userByToken = useCallback(() => dispatch(getUserByToken()), [dispatch])
   useEffect(() => {
     userByToken()
   }, [])
-
+  console.log(loading)
   return (
     <>
-      {isLoggedIn ? (
-        <Navigate to="/admin" />
+      {isRole && isLoggedIn ? (
+        'Admin' === isRole ? (
+          <Navigate to="/admin" />
+        ) : (
+          <Navigate to="/client" />
+        )
       ) : (
         <Routes>
           <Route element={<LoginPage />} path="login" />
