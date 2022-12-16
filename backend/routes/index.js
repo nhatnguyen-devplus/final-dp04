356958d32a4cc9a11245038f92bde2969e448447
nghialedp04 <nghia.le_dp04@devplus.edu.vn>
2022-12-16 11:02:45 +0700

    feat: api get list dayoffs

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ import LogOffRouter from './logOffRouter'
 import UserGroupRouter from './userGroupRouter'
 import UserRouter from './usersRouter'
 import NotificationRouter from './notificationRouter'
+import dayOffRouter from './dayOffRouter'
 const Routes = express.Router()
 
 Routes.use('/api/users', UserRouter)
@@ -11,5 +12,6 @@ Routes.use('/api/auth', AuthRouter)
 Routes.use('/api/user-groups', UserGroupRouter)
 Routes.use('/api/logoff', LogOffRouter)
 Routes.use('/api/notification', NotificationRouter)
+Routes.use('/api/dayoffs', dayOffRouter)
 
 export default Routes
