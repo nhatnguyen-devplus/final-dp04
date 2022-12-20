6e20c554b2edf5189997e9512c311ac05e28196f
nghialedp04 <nghia.le_dp04@devplus.edu.vn>
2022-12-20 16:09:05 +0700

    bugfix: delete user group

--- backend/validations/userValidation.js
+++ backend/validations/userValidation.js
@@ -1,11 +1,10 @@
 import Joi from 'joi'
-import { Role } from '../constants/enum'
 
 const checkUpdateReq = Joi.object({
   name: Joi.string().required().max(255),
   phone: Joi.string().required().max(10),
   email: Joi.string()
-    .pattern(new RegExp(/^[a-zA-Z0-9.-_]+@devplus.edu.v[n]$/))
+    .pattern(new RegExp(/^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-]+.[a-zA-Z.]*$/))
     .required()
     .max(255),
   role: Joi.string().required().valid('Staff', 'Admin'),
