82ff1cf3b905684191fcba51165ed2d804dce7f5
nghialedp04 <115454036+nghialedp04@users.noreply.github.com>
2022-12-22 18:01:22 +0700

    bugfix: update user (#74)

--- backend/validations/userValidation.js
+++ backend/validations/userValidation.js
@@ -2,12 +2,15 @@ import Joi from 'joi'
 
 const checkUpdateReq = Joi.object({
   name: Joi.string().required().max(255),
-  phone: Joi.string().required().max(10),
+  phone: Joi.string()
+    .pattern(new RegExp(/^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$/))
+    .required()
+    .min(10)
+    .max(10),
   email: Joi.string()
     .pattern(new RegExp(/^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-]+.[a-zA-Z.]*$/))
     .required()
     .max(255),
-  role: Joi.string().required().valid('Staff', 'Admin'),
 })
 
 export const userValidation = {
