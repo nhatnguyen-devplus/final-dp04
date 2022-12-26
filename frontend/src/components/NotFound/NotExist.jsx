9ff90c74cfef515363acbc22d94fd0fe95a6e25a
nghialedp04 <nghia.le_dp04@devplus.edu.vn>
2022-12-26 16:21:52 +0700

    feat: page not found

---
+++ frontend/src/components/NotFound/NotExist.jsx
@@ -0,0 +1,4 @@
+import { Result } from 'antd'
+import React from 'react'
+const NotExist = () => <Result status="404" subTitle="Sorry, the page you visited does not exist." title="404" />
+export default NotExist
